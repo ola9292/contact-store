46c241325d204f168534bccb6f273649f66f033c
olamide <okaz692@gmail.com>
2023-08-22 09:27:32 +0100

    links added

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import { useNavigate, Link } from "react-router-dom";
 
 export default function Navbar(){
     return(
         <div className="nav-box">
-           <h1>Contact Keeper</h1>
+            <Link to="/" style={{ textDecoration: 'none', color:'#fff' }}><h1>Contact Keeper</h1></Link>
            <ul className="nav-links">
-            <li>Create</li>
+           <Link to="/create" style={{ textDecoration: 'none', color:'#fff' }}>Create</Link>
             <li>Edit</li>
            </ul>
         </div>
